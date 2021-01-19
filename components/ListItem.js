354fa6407e68cbc1500ee70da5635ef5b1f4941d
Kentaro Suzuki <kentaro84207@gmail.com>
2021-01-20 08:11:16 +0900

    コンポーネントに切り出し

---
+++ components/ListItem.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainr: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+})
+
+const ListItem = () => {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainr}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: 'https://picsum.photos/id/19/200/200' }}
+        />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Text style={styles.subText}>ReactNews</Text>
+      </View>
+    </View>
+  )
+}
+
+export default ListItem

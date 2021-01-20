b38697c1f7f3a57115af69af878ff450b4545736
Kentaro Suzuki <kentaro84207@gmail.com>
2021-01-21 08:26:59 +0900

    News APIを取得

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native'
 import ListItem from './components/ListItem'
-import articles from './dummies/articles'
+import dummyArticles from './dummies/articles'
+import Constants from 'expo-constants'
+import axios from 'axios'
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`
 
 const styles = StyleSheet.create({
   container: {
@@ -12,6 +16,19 @@ const styles = StyleSheet.create({
 })
 
 export default function App() {
+  const [articles, setArticles] = useState(dummyArticles)
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL)
+      setArticles(response.data.articles)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
